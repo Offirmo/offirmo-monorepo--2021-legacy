4f0dcd1b34e682799dc5abd91f747e94276682b8
Offirmo <offirmo.net@gmail.com>
2018-04-16 22:47:17 +1000

    TLDW :scream_cat:

--- apps/hello-world-webpack-advanced/src/react-router-example.jsx
+++ apps/hello-world-webpack-advanced/src/react-router-example.jsx
@@ -3,13 +3,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+// auto-detect basename, correctly ignoring dynamic routes
 const BASE_ROUTE = (pathname => {
+	// stable point, everything after is likely to be a route
 	const TOP_SEGMENT_WE_ASSUME_WELL_BE_ALWAYS_SERVED_UNDER = WI_BASE_PATH
+
 	const splitted = pathname.split(TOP_SEGMENT_WE_ASSUME_WELL_BE_ALWAYS_SERVED_UNDER)
 	const parent_segment = splitted[0]
 	let base_route = parent_segment + TOP_SEGMENT_WE_ASSUME_WELL_BE_ALWAYS_SERVED_UNDER
 
-	// special dev/staging case
+	// special dev/staging case where we are served under an additional /dist
 	if (splitted[1].startsWith('/dist'))
 		base_route += '/dist'
 
