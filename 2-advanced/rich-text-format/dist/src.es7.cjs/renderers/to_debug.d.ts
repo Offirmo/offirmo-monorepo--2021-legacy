2e8c8ffcbab1b7207898ff661b4f393d951cc1ec
Offirmo <offirmo.net@gmail.com>
2018-08-27 08:26:49 +1000

    TLDW :scream_cat:

--- 2-advanced/rich-text-format/dist/src.es7.cjs/renderers/to_debug.d.ts
+++
@@ -1,8 +0,0 @@
-import { Node } from '../types';
-import { WalkerCallbacks } from '../walk';
-export declare type Options = {};
-export declare const DEFAULT_OPTIONS: {};
-declare type State = string;
-declare const callbacks: Partial<WalkerCallbacks<State, Options>>;
-declare function to_debug($doc: Node, options?: Options): string;
-export { callbacks, to_debug };
--- 2-advanced/rich-text-format/dist/src.es7/consts.d.ts
+++
@@ -1,6 +0,0 @@
-declare const LIB = "@offirmo/rich-text-format";
-declare const SCHEMA_VERSION: number;
-declare const NODE_TYPE_TO_DISPLAY_MODE: {
-    [k: string]: 'inline' | 'block';
-};
-export { LIB, SCHEMA_VERSION, NODE_TYPE_TO_DISPLAY_MODE, };
