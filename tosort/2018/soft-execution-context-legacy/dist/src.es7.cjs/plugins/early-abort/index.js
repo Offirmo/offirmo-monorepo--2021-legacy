fcdcb6331c27fc3f1ff8636247f3dc898891bced
Offirmo <offirmo.net@gmail.com>
2018-10-11 06:46:39 +1100

    TLDW :scream_cat:

--- tosort/2018/soft-execution-context-legacy/dist/src.es7.cjs/plugins/early-abort/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/*const state = {
-     aborted: false
- }*/
-//# sourceMappingURL=index.js.map
--- tosort/2018/soft-execution-context-legacy/dist/src.es7/constants.js
+++
@@ -1,4 +0,0 @@
-const LIB = 'soft-execution-context';
-const INTERNAL_PROP = '_';
-export { LIB, INTERNAL_PROP, };
-//# sourceMappingURL=constants.js.map
