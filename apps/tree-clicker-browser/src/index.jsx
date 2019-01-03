db527ad606e37b3d270b49f9d80d86a775207dd7
Offirmo <offirmo.net@gmail.com>
2019-01-04 01:43:52 +1000

    tree clicker bootstrap

---
+++ apps/tree-clicker-browser/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'babel-polyfill'
+import ReactDOM from 'react-dom'
+
+//import './services/raven'
+import './index.css'
+//import Root from './components/root'
+
+ReactDOM.render(
+	<p>
+		Hello
+	</p>,
+	document.getElementById('root'),
+)
