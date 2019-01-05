10a59a0e2d407c6540b3565ae52e1356bcadffe3
Offirmo <offirmo.net@gmail.com>
2019-01-06 08:57:05 +1000

    tree clicker ui

--- apps/tree-clicker-browser/src/index.tsx
+++ apps/tree-clicker-browser/src/index.tsx
@@ -4,14 +4,12 @@ import ReactDOM from 'react-dom'
 
 //import './services/raven'
 import './index.css'
-//import Root from './components/root'
+import Root from './components/app-root'
 
 const foo: string = 1
 
 console.log('Hello from ts2!', foo)
 ReactDOM.render(
-	<p>
-		Hello
-	</p>,
+	<Root />,
 	document.getElementById('root'),
 )
