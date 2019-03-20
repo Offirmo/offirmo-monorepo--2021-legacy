20a311b8994ef075a63411119bbfad99efd342d1
Offirmo <offirmo.net@gmail.com>
2019-03-20 18:35:29 +1100

    TLDW :scream_cat:

--- apps/the-boring-rpg/state/src/build.ts
+++ apps/the-boring-rpg/state/src/build.ts
@@ -1,4 +1,4 @@
 
 // THIS FILE IS AUTO GENERATED!
-export const VERSION = '0.58.3'
-export const BUILD_DATE = '20190306_23h29'
+export const VERSION = '0.58.4'
+export const BUILD_DATE = '20190320_07h32'
