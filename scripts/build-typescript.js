c80112d85f2bd0d4932a25f7e5de8347dff6e071
Offirmo <offirmo.net@gmail.com>
2019-07-05 21:46:42 +1000

    chore: improve build script: fix incorrect sub-dist folder name, allow legacy, remove useless option

--- scripts/build-typescript.js
+++ scripts/build-typescript.js
@@ -9,11 +9,11 @@ const tsc = require('../3-tools/node-typescript-compiler')
 
 const cli = meow('build', {
 	flags: {
-		watch: {
+		alsoLegacy: {
 			type: 'boolean',
 			default: false,
 		},
-		dev: {
+		watch: {
 			type: 'boolean',
 			default: false,
 		},
@@ -27,30 +27,26 @@ const DIST_DIR = path.join(PKG_PATH, 'dist')
 const PKG_JSON = require(path.join(PKG_PATH, 'package.json'))
 const PKG_NAME = PKG_JSON.name
 
-//const TSCONFIG_JSON = require(path.join(__dirname, '..', 'meta', 'tsconfig.json'))
+const TSCONFIG_JSON = require(path.join(__dirname, '..', 'meta', 'tsconfig.json'))
+const target = TSCONFIG_JSON.compilerOptions.target.toLowerCase()
 
 /////////////////////
 
-let compilerOptions = {} //TSCONFIG_JSON.compilerOptions
+let compilerOptions = {
+	// no need, automatically picked from closest tsconfig.json
+	//...TSCONFIG_JSON.compilerOptions
 
-compilerOptions = {
-	...compilerOptions,
 	//listFiles: true,
 	//listEmittedFiles: true,
 }
 
 if (cli.flags.watch) {
-	cli.flags.dev = true
-
 	compilerOptions = {
 		...compilerOptions,
+
 		watch: true,
-	}
-}
 
-if (cli.flags.dev) {
-	compilerOptions = {
-		...compilerOptions,
+		// it's dev mode, relax a bit:
 		noUnusedLocals: false,
 		noUnusedParameters: false,
 	}
@@ -58,44 +54,75 @@ if (cli.flags.dev) {
 
 /////////////////////
 
-//console.log({PKG_PATH, DIST_DIR, PKG_NAME, flags: cli.flags})
-console.log(`🛠  🔻 building ${stylize_string.bold(PKG_NAME)}...` + (cli.flags.dev ? ' (dev mode)' : ''))
-
-
-function build_compatible() {
+function build_legacy() {
+	const target = 'es5'
+	const out_dir = `src.${target}.cjs`
+	console.log(`      building ${PKG_NAME}/dist/${stylize_string.bold(out_dir)}`)
 	return tsc.compile(
 		{
 			...compilerOptions,
+			target,
 			module: 'commonjs',
-			outDir: path.join(DIST_DIR, 'src.es9.cjs'),
+			outDir: path.join(DIST_DIR, out_dir),
 			project: PKG_PATH,
 		},
 		null,
 		{
-			banner: `node-typescript-compiler compiling ${PKG_NAME}...`
+			banner: `node-typescript-compiler compiling ${PKG_NAME} to ./${out_dir}...`
+		},
+	)
+}
+
+function build_cjs() {
+	const out_dir = `src.${target}.cjs`
+	console.log(`      building ${PKG_NAME}/dist/${stylize_string.bold(out_dir)}`)
+	return tsc.compile(
+		{
+			...compilerOptions,
+			module: 'commonjs',
+			outDir: path.join(DIST_DIR, out_dir),
+			project: PKG_PATH,
+		},
+		null,
+		{
+			banner: `node-typescript-compiler compiling ${PKG_NAME} to ./${out_dir}...`
 		},
 	)
 }
 
 function build_esnext() {
+	const out_dir = `src.${target}`
+	console.log(`      building ${PKG_NAME}/dist/${stylize_string.bold(out_dir)}`)
 	return tsc.compile(
 		{
 			...compilerOptions,
-			outDir: path.join(DIST_DIR, 'src.es9'),
+			outDir: path.join(DIST_DIR, out_dir),
 			project: PKG_PATH,
 		},
 		null,
 		{
-			banner: `node-typescript-compiler compiling ${PKG_NAME}...`
+			banner: `node-typescript-compiler compiling ${PKG_NAME} to ./${out_dir}...`
 		},
 	)
 }
 
-// build sequentially to not duplicate the errors if any.
+/////////////////////
 
-build_compatible()
+//console.log({PKG_PATH, DIST_DIR, PKG_NAME, flags: cli.flags})
+console.log(`🛠  🔻 building ${stylize_string.bold(PKG_NAME)}...` + (cli.flags.watch ? ' (watch mode)' : ''))
+
+// build sequentially to not duplicate the errors if any.
+// CJS is usable in both node and bundled frontend,
+// thus we build only this one in watch = dev mode.
+build_cjs()
 	.then(() => {
-		if (cli.flags.dev) return
+		if (cli.flags.watch) return
+		if (!cli.flags.alsoLegacy) return
+
+		return build_legacy()
+	})
+	.then(() => {
+		if (cli.flags.watch) return
 
 		return build_esnext()
 	})
