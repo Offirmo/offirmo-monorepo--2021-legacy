ddaa483c15cac8fe2956e51d68ce7fc5816c964a
Offirmo <offirmo.net@gmail.com>
2019-10-02 17:14:21 +1000

    rework functions build process

--- apps/shared/functions/README.md
+++ apps/shared/functions/README.md
@@ -3,6 +3,8 @@
 ## dev
 doc:
 * https://www.netlify.com/docs/functions/
+  * Selecting the JS runtime: https://www.netlify.com/docs/functions/#javascript-runtime-settings
+  * 2019/10/01 latest Allowed = 10.16.3
 * https://serverless.com/blog/common-node8-mistakes-in-lambda/
 * status codes https://www.codetinkerer.com/2015/12/04/choosing-an-http-status-code.html
 
---
+++ apps/shared/functions/webpack.config.ts
@@ -0,0 +1,13 @@
+// https://github.com/netlify/netlify-lambda#webpack-configuration
+
+// https://github.com/liady/webpack-node-externals
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+	//target: 'node', // in order to ignore built-in modules like path, fs, etc.
+	externals: [
+		nodeExternals({
+			whitelist: [ /^offirmo/, /^tbrpg/ ],
+		}),
+	],
+};
