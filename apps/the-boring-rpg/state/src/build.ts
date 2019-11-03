56510d1476721fe6d433c3b1799926c68c5e2586
Offirmo <offirmo.net@gmail.com>
2019-11-04 04:59:38 +1100

    rebuild

--- apps/the-boring-rpg/state/src/build.ts
+++ apps/the-boring-rpg/state/src/build.ts
@@ -1,5 +1,5 @@
 
 // THIS FILE IS AUTO GENERATED!
-export const VERSION: string = '0.60.12'
-export const NUMERIC_VERSION: number = 60.12 // for easy comparisons
-export const BUILD_DATE: string = '20191101_21h14'
+export const VERSION: string = '0.60.13'
+export const NUMERIC_VERSION: number = 60.13 // for easy comparisons
+export const BUILD_DATE: string = '20191103_17h54'
