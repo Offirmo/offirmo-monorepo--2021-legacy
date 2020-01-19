80db4aed9feb5c13510abbb773ebf5ff8fc284b7
Offirmo <offirmo.net@gmail.com>
2020-01-19 19:40:08 +1100

    timestamps: use padStart, new methods, new comments

--- 1-stdlib/timestamps/src/generate.ts
+++ 1-stdlib/timestamps/src/generate.ts
@@ -12,41 +12,63 @@ import {
 	HumanReadableTimestampUTCMs,
 } from './types'
 
+// ex. 1542780045627
 function get_UTC_timestamp_ms(now: Readonly<Date> = new Date()): TimestampUTCMs {
 	return (+now)
 }
 
 /////////////////////
+// spec:
+// - human readable
+// - as short as possible
 
+// ex. 20181121
+// assumed side effect of being castable to a number
 function get_human_readable_UTC_timestamp_days(now: Readonly<Date> = new Date()): HumanReadableTimestampUTCDays {
 	const YYYY = now.getUTCFullYear()
-	const MM = ('0' + (now.getUTCMonth() + 1)).slice(-2)
-	const DD = ('0' + now.getUTCDate()).slice(-2)
+	const MM = String(now.getUTCMonth() + 1).padStart(2, '0')
+	const DD = String(now.getUTCDate()).padStart(2, '0')
 
 	return `${YYYY}${MM}${DD}`
 }
 
+// ex. 20181121_06h00
 function get_human_readable_UTC_timestamp_minutes(now: Readonly<Date> = new Date()): HumanReadableTimestampUTCMinutes {
-	const hh = ('0' + now.getUTCHours()).slice(-2)
-	const mm = ('0' + now.getUTCMinutes()).slice(-2)
+	const hh = String(now.getUTCHours()).padStart(2, '0')
+	const mm = String(now.getUTCMinutes()).padStart(2, '0')
 
 	return get_human_readable_UTC_timestamp_days(now) + `_${hh}h${mm}`
 }
 
 function get_human_readable_UTC_timestamp_seconds(now: Readonly<Date> = new Date()): HumanReadableTimestampUTCSeconds {
-	const ss = ('0' + now.getUTCSeconds()).slice(-2)
+	const ss = String(now.getUTCSeconds()).padStart(2, '0')
 
 	return get_human_readable_UTC_timestamp_minutes(now) + `+${ss}`
 }
 
 function get_human_readable_UTC_timestamp_ms(now: Readonly<Date> = new Date()): HumanReadableTimestampUTCMs {
-	const mmm = ('00' + now.getUTCMilliseconds()).slice(-3)
+	const mmm = String(now.getUTCMilliseconds()).padStart(3, '0')
 
 	return get_human_readable_UTC_timestamp_seconds(now) + `.${mmm}`
 }
 
 /////////////////////
 
+// ISO 8601 Extended Format. The format is as follows: YYYY-MM-DDTHH:mm:ss.sssZ
+// http://www.ecma-international.org/ecma-262/5.1/#sec-15.9.1.15
+function get_ISO8601_extended_ms(now: Readonly<Date> = new Date()): HumanReadableTimestampUTCMs {
+	return now.toISOString()
+}
+
+function get_ISO8601_simplified_minutes(now: Readonly<Date> = new Date()): HumanReadableTimestampUTCMs {
+	return get_ISO8601_extended_ms(now).slice(0, 16)
+}
+
+function get_ISO8601_simplified_day(now: Readonly<Date> = new Date()): HumanReadableTimestampUTCMs {
+	return get_ISO8601_extended_ms(now).slice(0, 10)
+}
+
+// fun but unclear
 // https://space.stackexchange.com/questions/36628/utc-timestamp-format-for-launch-vehicles
 /*function get_space_timestamp_ms(now: Readonly<Date> = new Date()): string {
 	const YYYY = now.getUTCFullYear()
@@ -69,6 +91,10 @@ export {
 	get_human_readable_UTC_timestamp_seconds,
 	get_human_readable_UTC_timestamp_minutes,
 	get_human_readable_UTC_timestamp_days,
+
+	get_ISO8601_extended_ms,
+	get_ISO8601_simplified_minutes,
+	get_ISO8601_simplified_day,
 }
 
 /////////////////////
