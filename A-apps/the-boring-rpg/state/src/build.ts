5e5604a942d0c68872ca0b591934e7683db5d87b
Offirmo <offirmo.net@gmail.com>
2020-05-21 21:55:21 +1000

    +++

--- A-apps/the-boring-rpg/state/src/build.ts
+++ A-apps/the-boring-rpg/state/src/build.ts
@@ -1,5 +1,5 @@
 
 // THIS FILE IS AUTO GENERATED!
-export const VERSION: string = '0.62.1'
-export const NUMERIC_VERSION: number = 0.6201 // for easy comparisons
-export const BUILD_DATE: string = '20200506_04h35'
+export const VERSION: string = '0.62.2'
+export const NUMERIC_VERSION: number = 0.6202 // for easy comparisons
+export const BUILD_DATE: string = '20200521_11h52'
