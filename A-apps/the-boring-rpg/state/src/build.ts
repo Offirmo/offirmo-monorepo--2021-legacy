358757d4ad32a32f7454d4705d57c9d560e62a31
Offirmo <offirmo.net@gmail.com>
2020-11-05 06:31:03 +1100

    bump and rebuild

--- A-apps/the-boring-rpg/state/src/build.ts
+++ A-apps/the-boring-rpg/state/src/build.ts
@@ -1,5 +1,5 @@
 
 // THIS FILE IS AUTO GENERATED!
-export const VERSION: string = '0.65.11'
-export const NUMERIC_VERSION: number = 0.6511 // for easy comparisons
-export const BUILD_DATE: string = '20201008_09h44'
+export const VERSION: string = '0.65.12'
+export const NUMERIC_VERSION: number = 0.6512 // for easy comparisons
+export const BUILD_DATE: string = '20201104_10h26'
