9da6ba4df684272e82775c5b6bbbc5fd65ebd3d0
Offirmo <offirmo.net@gmail.com>
2021-05-17 17:15:58 +1000

    +++

--- 3-advanced--browser/storybook--demos/.storybook/main.js
+++ 3-advanced--browser/storybook--demos/.storybook/main.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
+	'stories': [
+		'../stories/**/*.stories.mdx',
+		'../stories/**/*.stories.@(js|jsx|ts|tsx)',
+	],
+	'addons': [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+	],
 	reactOptions: {
 		fastRefresh: true,
 		strictMode: true,
