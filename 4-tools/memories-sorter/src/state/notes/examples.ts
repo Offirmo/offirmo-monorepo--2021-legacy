0bc751ab3b66e0ffdd82e9bf4cd0af0e80b4be2c
Offirmo <offirmo.net@gmail.com>
2021-08-21 14:48:54 +1000

    +++

--- 4-tools/memories-sorter/src/state/notes/examples.ts
+++ 4-tools/memories-sorter/src/state/notes/examples.ts
@@ -1,7 +1,9 @@
 import { Immutable, enforce_immutability } from '@offirmo-private/state-utils'
 
+import { SCHEMA_VERSION } from './consts'
 import { State } from './types'
 import { PersistedNotes as FileNotes } from '../file'
+import { Basename, SimpleYYYYMMDD } from '../../types'
 
 /////////////////////
 
@@ -11,12 +13,97 @@ import { PersistedNotes as FileNotes } from '../file'
 // needed for demos
 
 const DEMO_STATE: Immutable<State> = enforce_immutability<State>({
-	"_comment": "This data is from @offirmo/photo-sorter https://github.com/Offirmo/offirmo-monorepo/tree/master/5-incubator/active/photos-sorter",
-	"schema_version": 1,
-	"revision": 0,
-	"last_user_investment_tms": 1613965853218,
-	"encountered_files": {
-		// TODO
+	_comment: 'This data is from @offirmo/photo-sorter https://github.com/Offirmo/offirmo-monorepo/tree/master/5-incubator/active/photos-sorter',
+	schema_version: SCHEMA_VERSION,
+	revision: 0,
+	last_user_investment_tms: 1613965853218,
+
+	'encountered_files': {
+
+		'1a78788de0e03b60824166b7e17a86668c052067655c54842c0abcb484c2ab93': {
+			historical: {
+				'basename': 'Image from iOS (15).jpg',
+				'parent_path': '',
+				'fs_bcd_tms': 1571961828388,
+				'neighbor_hints': {
+					'fs_bcd_assessed_reliability': 'unknown'
+				},
+			},
+			// user data
+			deleted: undefined,
+			starred: undefined,
+			manual_date: undefined,
+			// meta
+			best_date_afawk_symd: undefined,
+			// debug
+			currently_known_as: 'Image from iOS (15).jpg',
+			renaming_source: undefined,
+		},
+
+		'ce6b3d3128a28331db2a25c42671a3793af557454165aa5a1d56c71160cb291c': {
+			historical: {
+				'basename': 'IMG_1474.JPG',
+				'parent_path': '',
+				'fs_bcd_tms': 1566873905000,
+				'neighbor_hints': {
+					'fs_bcd_assessed_reliability': 'unreliable'
+				},
+				'exif_orientation': 6
+			},
+			// user data
+			deleted: undefined,
+			starred: undefined,
+			manual_date: undefined,
+			// meta
+			best_date_afawk_symd: undefined,
+			// debug
+			'currently_known_as': 'MM2019-08-27_12h45m05s158_IMG_1474.jpg',
+			'renaming_source': 'exif'
+		},
+
+
+		'324c096dc39b96d71a73bfa558f0bdddb708c226f0f13bb231a548208257b857': {
+			historical: {
+				'basename': 'CLIP0003.AVI',
+				'parent_path': '2003/88- St Gilles',
+				'fs_bcd_tms': 1052375074000,
+				'neighbor_hints': {
+					'fs_bcd_assessed_reliability': 'reliable'
+				}
+			},
+
+			// user data
+			deleted: undefined,
+			starred: undefined,
+			manual_date: undefined,
+			// meta
+			best_date_afawk_symd: undefined,
+			// debug
+			currently_known_as: 'CLIP0003.avi',
+			renaming_source: 'historical_fs+historical_env_hints',
+		},
+
+		'fb7b013a830e451de2dfc444d5eaa9af9f67e1eb7f542a422d835cd3123d5e29': {
+			historical: {
+				basename: 'MM2020-01-01_17h57m29s625_IMG_8763.jpg',
+				parent_path: '2020/20200101 - life',
+				fs_bcd_tms: 1581630576844,
+				'neighbor_hints': {
+					'fs_bcd_assessed_reliability': 'unknown'
+				},
+			},
+
+			// user data
+			deleted: undefined,
+			starred: undefined,
+			manual_date: undefined,
+			// meta
+			best_date_afawk_symd: undefined,
+			// debug
+			currently_known_as: 'MM2020-01-01_17h57m29s625_IMG_8763.jpg',
+			renaming_source: undefined,
+		},
+
 	},
 
 	known_modifications_new_to_old: {
--- 4-tools/memories-sorter/src/state/notes/migrations.ts
+++ 4-tools/memories-sorter/src/state/notes/migrations.ts
@@ -1,3 +1,4 @@
+import assert from 'tiny-invariant'
 import { Immutable, enforce_immutability } from '@offirmo-private/state-utils'
 import {
 	LastMigrationStep,
@@ -8,6 +9,9 @@ import {
 import { LIB, SCHEMA_VERSION } from './consts'
 import { State } from './types'
 import { SoftExecutionContext } from '../../services/sec'
+import { Basename, RelativePath, SimpleYYYYMMDD } from '../../types'
+import { FsReliability, HistoricalData, PersistedNotes as FileNotes } from '../file'
+import { BetterDateMembers } from '../../services/better-date'
 
 // some hints may be needed to migrate to demo state
 // need to export them for composing tests
@@ -40,12 +44,90 @@ const migrate_to_2x: LastMigrationStep<StateForMigration, any> = (SEC, legacy_st
 	if (legacy_schema_version < 1)
 		legacy_state = next(SEC, legacy_state, hints)
 
-	let state: any = {
-		...legacy_state,
+	assert(
+		Object.keys(legacy_state).sort().join(',')
+		=== '_comment,encountered_files,known_modifications_new_to_old,last_user_action_tms,revision,schema_version',
+		`migrate_to_2x() unexpected keys: ` + Object.keys(legacy_state).sort().join(',')
+	)
+
+	let { last_user_action_tms, ...rest } = legacy_state
+	let state: StateForMigration = {
+		...rest,
 		schema_version: 2,
+		last_user_investment_tms: last_user_action_tms,
+		encountered_files: {}, // temporarily
 	}
 
-	throw new Error('migrate_to_2x NIMP!')
+	Object.keys(legacy_state.encountered_files).forEach(hash => {
+		const legacy_file_notes: any = legacy_state.encountered_files[hash]
+		const migrated_notes = {
+			deleted: undefined,
+			starred: undefined,
+			manual_date: undefined,
+			best_date_afawk_symd: undefined,
+			renaming_source: undefined,
+			currently_known_as: '',
+			historical: {
+				basename: '',
+				parent_path: '',
+				fs_bcd_tms: 0,
+				neighbor_hints: {
+					fs_bcd_assessed_reliability: 'unknown',
+					parent_folder_bcd: undefined,
+				},
+				exif_orientation: undefined,
+				trailing_extra_bytes_cleaned: undefined,
+			},
+		} as FileNotes
+
+		let keys = Object.keys(legacy_file_notes)
+		if (keys.includes('currently_known_as')) {
+			migrated_notes.currently_known_as = legacy_file_notes.currently_known_as
+			keys = keys.filter(k => k !== 'currently_known_as')
+		}
+		if (keys.includes('renaming_source')) {
+			migrated_notes.renaming_source = legacy_file_notes.renaming_source
+			keys = keys.filter(k => k !== 'renaming_source')
+		}
+		if (keys.includes('original')) {
+
+			let subkeys = Object.keys(legacy_file_notes.original)
+			if (subkeys.includes('basename')) {
+				migrated_notes.historical.basename = legacy_file_notes.original.basename
+				subkeys = subkeys.filter(k => k !== 'basename')
+			}
+			if (subkeys.includes('parent_path')) {
+				migrated_notes.historical.parent_path = legacy_file_notes.original.parent_path
+				subkeys = subkeys.filter(k => k !== 'parent_path')
+			}
+			if (subkeys.includes('fs_birthtime_ms')) {
+				migrated_notes.historical.fs_bcd_tms = legacy_file_notes.original.fs_birthtime_ms
+				subkeys = subkeys.filter(k => k !== 'fs_birthtime_ms')
+			}
+			if (subkeys.includes('exif_orientation')) {
+				migrated_notes.historical.exif_orientation = legacy_file_notes.original.exif_orientation
+				subkeys = subkeys.filter(k => k !== 'exif_orientation')
+			}
+			if (subkeys.includes('is_fs_birthtime_assessed_reliable')) {
+				migrated_notes.historical.neighbor_hints.fs_bcd_assessed_reliability =
+					legacy_file_notes.original.is_fs_birthtime_assessed_reliable ? 'reliable' : 'unknown'
+				subkeys = subkeys.filter(k => k !== 'is_fs_birthtime_assessed_reliable')
+			}
+
+			console.assert(
+				subkeys.length === 0,
+				`migrate_to_2x() unexpected subkey for encountered_files: ${subkeys.join(',')}!`
+			)
+
+			keys = keys.filter(k => k !== 'original')
+		}
+		console.assert(
+			keys.length === 0,
+			`migrate_to_2x() unexpected key for encountered_files: ${keys.join(',')}!`
+		)
+
+		state.encountered_files[hash] = migrated_notes
+	})
 
 	return state
 }
