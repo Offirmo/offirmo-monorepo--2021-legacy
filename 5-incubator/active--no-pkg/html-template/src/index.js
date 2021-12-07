43a7a8cfc77f77ff3df0828d3366c133581fdc74
Offirmo <offirmo.net@gmail.com>
2021-12-07 14:52:55 +1100

    +++

---
+++ 5-incubator/active--no-pkg/html-template/src/index.js
@@ -0,0 +1,12 @@
+
+
+class SocialMediaButton extends HTMLButtonElement {
+	constructor() {
+		// Always call super first in constructor
+		super()
+
+		// Element functionality written in here
+
+	...
+	}
+}
