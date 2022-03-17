70ec90bca15297ec4327189d693b7efb758706f3
Offirmo <offirmo.net@gmail.com>
2022-03-17 16:43:57 +1100

    +++

---
+++ stack--next/C-apps--clients/immortal-clicker/client--browser/src/app.ts
@@ -0,0 +1,8 @@
+
+import { add } from '@offirmo-private/example--module--ts'
+
+import * as _ from 'lodash'
+
+console.log('Hello world!')
+console.log(`Hello ${_.upperCase('world')}!`)
+console.log('2 + 2 =', add(2, 2))
